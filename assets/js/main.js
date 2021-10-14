b03845945a52ca1b119487329ddecd86ce2aeba5
pizzi9523 <pizzitolagiuseppe@live.it>
2021-10-14 15:58:50 +0200

    Stampo messaggio appropriato sul file html

--- assets/js/main.js
+++ assets/js/main.js
@@ -11,16 +11,14 @@ const flag = emailsAllowed.includes(userEmail);
 //console.log(flag);
 
 //stampo un messaggio appropriato sull'esito del controllo
+const messageElement = document.getElementById("access");
 if (flag == true) {
-    console.log("Accesso Consentito");
+    //console.log("Accesso Consentito");
+    messageElement.innerHTML = "Accesso Consentito"
+    messageElement.style.color = "green"
 }
 else {
-    console.log("Accesso Negato");
+    //console.log("Accesso Negato");
+    messageElement.innerHTML = "Accesso Negato"
+    messageElement.style.color = "red"
 }
-
-
-
-
-
-
-
