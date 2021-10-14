870bf1201e574aa6b56b83cf3aeb7b04b2b85aa4
pizzi9523 <pizzitolagiuseppe@live.it>
2021-10-14 15:52:03 +0200

    check email allowed e stampo messaggio appropriato

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,7 +6,17 @@ const userEmail = prompt("Inserisci la tua email: (es. nome@email.it)");
 const emailsAllowed = ["giuseppe@email.it", "fabio@email.it", "tiziano@email.it", "fabiola@email.it", "chiara@email.it"];
 //console.log(emailsAllowed);
 
+//controllo che sia nella lista di chi può accedere
+const flag = emailsAllowed.includes(userEmail);
+//console.log(flag);
 
+//stampo un messaggio appropriato sull'esito del controllo
+if (flag == true) {
+    console.log("Accesso Consentito");
+}
+else {
+    console.log("Accesso Negato");
+}
 
 
 
