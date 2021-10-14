7bde4afcc6de500c912c7fbe06eb270bee88a510
pizzi9523 <pizzitolagiuseppe@live.it>
2021-10-14 16:34:18 +0200

    Stampo a schermo le scelte di pc e user e l'esito

--- assets/js/main.js
+++ assets/js/main.js
@@ -14,31 +14,50 @@ const flag = emailsAllowed.includes(userEmail);
 const messageElement = document.getElementById("access");
 if (flag == true) {
     //console.log("Accesso Consentito");
-    messageElement.innerHTML = "Accesso Consentito"
-    messageElement.style.color = "green"
+    messageElement.innerHTML = "Accesso Consentito";
+    messageElement.style.color = "green";
 }
 else {
     //console.log("Accesso Negato");
-    messageElement.innerHTML = "Accesso Negato"
-    messageElement.style.color = "red"
+    messageElement.innerHTML = "Accesso Negato";
+    messageElement.style.color = "red";
 }
 
+
+const containerElement = document.querySelector(".container");
 //Generare un numero random da 1 a 6, sia per il giocatore sia per il computer
+const userChoiceElement = document.createElement("p");
+const pcChoiceElement = document.createElement("p");
+containerElement.append(userChoiceElement);
+containerElement.append(pcChoiceElement);
+
+userChoiceElement.innerHTML = "L'User ha scelto il numero: ";
+pcChoiceElement.innerHTML = "Il PC ha scelto il numero: ";
+
 
 const userRandomNumber = Math.floor(Math.random() * 6) + 1;
 const pcRandomNumber = Math.floor(Math.random() * 6) + 1;
 // console.log(userRandomNumber);
 // console.log(pcRandomNumber);
-
-
+userChoiceElement.append(userRandomNumber);
+pcChoiceElement.append(pcRandomNumber);
 
 //Stabilire il vincitore in base al punteggio più alto
+const esitoElement = document.createElement("h2");
+containerElement.append(esitoElement)
+
 if (userRandomNumber > pcRandomNumber) {
     console.log("User hai vinto");
+    esitoElement.innerHTML = "User hai vinto";
+
 }
 else if (userRandomNumber < pcRandomNumber) {
     console.log("PC hai vinto");
+    esitoElement.innerHTML = "Pc hai vinto";
+
 }
 else {
     console.log("Avete pareggiato");
+    esitoElement.innerHTML = "Avete pareggiato";
+
 }
