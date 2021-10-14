919b83c5d09948605da14cdb599ea42e69267f31
pizzi9523 <pizzitolagiuseppe@live.it>
2021-10-14 15:48:47 +0200

    Chiedo all'utente l' email, creo una lista di mail

---
+++ assets/js/main.js
@@ -0,0 +1,16 @@
+//Chiedo all'utente la sua email
+const userEmail = prompt("Inserisci la tua email: (es. nome@email.it)");
+//console.log(userEmail);
+
+//Creo una lista di email consentiti
+const emailsAllowed = ["giuseppe@email.it", "fabio@email.it", "tiziano@email.it", "fabiola@email.it", "chiara@email.it"];
+//console.log(emailsAllowed);
+
+
+
+
+
+
+
+
+
